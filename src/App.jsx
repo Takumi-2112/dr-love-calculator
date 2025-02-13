b2f473396ff2500a6f346918719dc6d17844b7f5
Tommy <azran.thomas@gmail.com>
2025-02-13 15:43:36 -0500

    phone test 8

--- src/App.jsx
+++ src/App.jsx
@@ -40,8 +40,4 @@ function App() {
         {touchButtonOne > 1 && touchButtonTwo > 1 ? <div className='dr-love-div'><img className="dr-love-img" src={drLove} alt="" /></div> : null}
       </div>
       <TouchButtonTwo touchButtonTwoToggler={touchButtonTwoToggler}/>
-    </div>
-  );
-}
-
-export default App;
+    </div
