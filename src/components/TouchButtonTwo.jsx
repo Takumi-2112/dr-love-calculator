6e56597729ae677f27a97a055def9a073300cf01
Tommy <azran.thomas@gmail.com>
2025-02-13 10:34:00 -0500

    test phone 3

--- src/components/TouchButtonTwo.jsx
+++ src/components/TouchButtonTwo.jsx
@@ -1,7 +1,14 @@
 import "../styles/TouchButtonTwo.css";
+
 function TouchButtonTwo({ touchButtonTwoToggler }) {
   return (
-    <button className="finger-button-two" onClick={touchButtonTwoToggler}><i className="fa-solid fa-fingerprint finger-two"></i></button>
+    <button 
+      className="finger-button-two" 
+      onClick={touchButtonTwoToggler} 
+      aria-label="Toggle Fingerprint"
+    >
+      <i className="fa-solid fa-fingerprint finger-two"></i>
+    </button>
   );
 }
 
