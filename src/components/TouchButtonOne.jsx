f9629b8bf99771912169149194605b0c194110ed
Tommy <azran.thomas@gmail.com>
2025-02-14 10:26:31 -0500

    left margin test

--- src/components/TouchButtonOne.jsx
+++ src/components/TouchButtonOne.jsx
@@ -9,30 +9,4 @@ function TouchButtonOne({ touchButtonOneToggler, touchButtonOne }) {
     let currentProgress = 0;
 
     const interval = setInterval(() => {
-      currentProgress += 10; // Increase by 10% every 100ms
-      setProgress(currentProgress);
-
-      if (currentProgress >= 100) {
-        clearInterval(interval);
-        touchButtonOneToggler(); // Trigger action when full
-      }
-    }, 100);
-  };
-
-  return (
-    <div className="finger-button-one-container">
-      <button
-        className="finger-button-one"
-        onTouchStart={handleTouch} // No more touch hold needed
-        aria-label="Toggle Fingerprint"
-      >
-        <i className="fa-solid fa-fingerprint finger-one"></i>
-      </button>
-      <div className="progress-bar-one">
-        <div className="progress-fill-one" style={{ width: `${progress}%` }}></div>
-      </div>
-    </div>
-  );
-}
-
-export default TouchButtonOne;
+      currentProgress
