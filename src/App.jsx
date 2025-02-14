59ceed1c6eedb5f4974eed3d5d57a2d7d05e7b24
Tommy <azran.thomas@gmail.com>
2025-02-14 15:36:49 -0500

    modal test 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,158 @@
-import { useState, useEffect } from 'react';
-import './styles/App.css';
-import welcomeDr from '../public/welcome-dr.png'; 
-import drGene from '../public/dr-gene.png';
-import drLove from '../public/dr-love.png';
-import VdayModal from './components/VdayModal';
-import TouchButtonOne from './components/TouchButtonOne';
-import TouchButtonTwo from './components/TouchButtonTwo';
-
+import { useState, useEffect } from "react";
+import "./styles/App.css";
+import welcomeDr from "../public/welcome-dr.png";
+import drGene from "../public/dr-gene.png";
+import drLove from "../public/dr-love.png";
+import VdayModal from "./components/VdayModal";
+import TouchButtonOne from "./components/TouchButtonOne";
+import TouchButtonTwo from "./components/TouchButtonTwo";
 
 function App() {
-  const [vdayModal, setVdayModal] = useState(true);
+  const [vdayModal, setVdayModal] = useState(false);
   const [touchButtonOne, setTouchButtonOne] = useState(0);
   const [touchButtonTwo, setTouchButtonTwo] = useState(0);
 
   useEffect(() => {
+    if ((touchButtonOne === 1 && touchButtonTwo === 1) || (touchButtonOne === 2 && touchButtonTwo === 2)) {
+      const timer = setTimeout(() => {
+        setVdayModal(true);
+      }, 1500); // 2-second delay
+  
+      return () => clearTimeout(timer); // Cleanup in case state changes before timeout finishes
+    }
     console.log(`Touch Button One state updated to: ${touchButtonOne}`);
     console.log(`Touch Button Two state updated to: ${touchButtonTwo}`);
-
   }, [touchButtonOne, touchButtonTwo]);
 
-  const vdayModalToggler = () => {
-    setVdayModal(prevState => !prevState);
+
+  const closeVdayModal = () => {
+    setVdayModal((prevState) => !prevState);
   };
 
   const touchButtonOneToggler = () => {
-    setTouchButtonOne(prevState => prevState + 1);
+    setTouchButtonOne((prevState) => prevState + 1);
   };
 
   const touchButtonTwoToggler = () => {
-    setTouchButtonTwo(prevState => prevState + 1);
+    setTouchButtonTwo((prevState) => prevState + 1);
   };
 
   return (
     <div className="app-master">
-      {vdayModal && <VdayModal touchButtonOne={touchButtonOne} touchButtonTwo={touchButtonTwo} vdayModalToggler={vdayModalToggler}/>}
-      <TouchButtonOne touchButtonOneToggler={touchButtonOneToggler} touchButtonOne={touchButtonOne}/>
-      <div className="content-div">  
-        <div className='title-div'>
-          <h1 className='title'>DR LOVE'S <span className='vday'>VDAY</span> CALCULATOR</h1>
-        </div>
-        {touchButtonOne === 0 && touchButtonTwo === 0 ? <div className='welcome-dr-div'><img className="welcome-dr-img" src={welcomeDr} alt="" /></div> : null}
-        {touchButtonOne === 1 && touchButtonTwo === 0 ? <div className='welcome-dr-div'><img className="welcome-dr-img" src={welcomeDr} alt="" /></div> : null}
-        {touchButtonOne === 0 && touchButtonTwo === 1 ? <div className='welcome-dr-div'><img className="welcome-dr-img" src={welcomeDr} alt="" /></div> : null}
-        {touchButtonOne === 1 && touchButtonTwo === 1 ? <div className='gene-div'><img className="gene-img" src={drGene} alt="" /></div> : null}
-        {touchButtonOne > 1 && touchButtonTwo === 1 ? <div className='gene-div'><img className="gene-img" src={drGene} alt="" /></div> : null}
-        {touchButtonOne === 1 && touchButtonTwo > 1 ? <div className='gene-div'><img className="gene-img" src={drGene} alt="" /></div> : null}
-        {touchButtonOne > 1 && touchButtonTwo > 1 ? <div className='dr-love-div'><img className="dr-love-img" src={drLove} alt="" /></div> : null}
-      </div>
-      <TouchButtonTwo touchButtonTwoToggler={touchButtonTwoToggler} touchButtonTwo={touchButtonTwo}/>
+      {touchButtonOne >= 1 && touchButtonTwo >= 1 ? (
+        <>
+          {vdayModal && (
+            <VdayModal
+              touchButtonOne={touchButtonOne}
+              touchButtonTwo={touchButtonTwo}
+              closeVdayModal={closeVdayModal}
+            />
+          )}
+          <TouchButtonOne
+            touchButtonOneToggler={touchButtonOneToggler}
+            touchButtonOne={touchButtonOne}
+          />
+          <div className="content-div">
+            <div className="title-div">
+              <h1 className="title">
+                DR LOVE'S <span className="vday">VDAY</span> CALCULATOR
+              </h1>
+            </div>
+            {touchButtonOne === 0 && touchButtonTwo === 0 ? (
+              <div className="welcome-dr-div">
+                <img className="welcome-dr-img" src={welcomeDr} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 1 && touchButtonTwo === 0 ? (
+              <div className="welcome-dr-div">
+                <img className="welcome-dr-img" src={welcomeDr} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 0 && touchButtonTwo === 1 ? (
+              <div className="welcome-dr-div">
+                <img className="welcome-dr-img" src={welcomeDr} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 1 && touchButtonTwo === 1 ? (
+              <div className="gene-div">
+                <img className="gene-img" src={drGene} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne > 1 && touchButtonTwo === 1 ? (
+              <div className="gene-div">
+                <img className="gene-img" src={drGene} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 1 && touchButtonTwo > 1 ? (
+              <div className="gene-div">
+                <img className="gene-img" src={drGene} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne > 1 && touchButtonTwo > 1 ? (
+              <div className="dr-love-div">
+                <img className="dr-love-img" src={drLove} alt="" />
+              </div>
+            ) : null}
+          </div>
+          <TouchButtonTwo
+            touchButtonTwoToggler={touchButtonTwoToggler}
+            touchButtonTwo={touchButtonTwo}
+          />
+        </>
+      ) : (
+        <>
+          <TouchButtonOne
+            touchButtonOneToggler={touchButtonOneToggler}
+            touchButtonOne={touchButtonOne}
+          />
+          <div className="content-div">
+            <div className="title-div">
+              <h1 className="title">
+                DR LOVE'S <span className="vday">VDAY</span> CALCULATOR
+              </h1>
+            </div>
+            {touchButtonOne === 0 && touchButtonTwo === 0 ? (
+              <div className="welcome-dr-div">
+                <img className="welcome-dr-img" src={welcomeDr} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 1 && touchButtonTwo === 0 ? (
+              <div className="welcome-dr-div">
+                <img className="welcome-dr-img" src={welcomeDr} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 0 && touchButtonTwo === 1 ? (
+              <div className="welcome-dr-div">
+                <img className="welcome-dr-img" src={welcomeDr} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 1 && touchButtonTwo === 1 ? (
+              <div className="gene-div">
+                <img className="gene-img" src={drGene} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne > 1 && touchButtonTwo === 1 ? (
+              <div className="gene-div">
+                <img className="gene-img" src={drGene} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne === 1 && touchButtonTwo > 1 ? (
+              <div className="gene-div">
+                <img className="gene-img" src={drGene} alt="" />
+              </div>
+            ) : null}
+            {touchButtonOne > 1 && touchButtonTwo > 1 ? (
+              <div className="dr-love-div">
+                <img className="dr-love-img" src={drLove} alt="" />
+              </div>
+            ) : null}
+          </div>
+          <TouchButtonTwo
+            touchButtonTwoToggler={touchButtonTwoToggler}
+            touchButtonTwo={touchButtonTwo}
+          />
+        </>
+      )}
     </div>
   );
 }
--- src/components/VdayModal.jsx
+++ src/components/VdayModal.jsx
@@ -2,18 +2,24 @@ import "../styles/VdayModal.css";
 import gengar from "../assets/gengar.png";
 import kirby from "../assets/kirby.png";
 
-function VdayModal({ touchButtonOne, touchButtonTwo, vdayModalToggler }) {
+function VdayModal({ touchButtonOne, touchButtonTwo, closeVdayModal }) {
   return (
     <div className="modal">
       <div className="modal-overlay">
         <div className="modal-content">
           <div className="modal-close-container">
-            <h3>k</h3>
-            <h2 className="login-title"><img className="gengar" src={gengar} alt="" />1000% MATCH BABAY <img className="kirby" src={kirby} alt="" /></h2>
-            <i onClick={vdayModalToggler} className="fa-solid fa-xmark close-modal"></i>
+            <h3 className="invisible">k</h3>
+            {touchButtonOne === 1 && touchButtonTwo === 1 ? <h2 className="login-title">❌2% MATCH...😬</h2> : null}
+                    {touchButtonOne > 1 && touchButtonTwo === 1 ? <h2 className="login-title">❌2% MATCH...😬</h2> : null}
+                    {touchButtonOne === 1 && touchButtonTwo > 1 ? <h2 className="login-title">❌2% MATCH...😬</h2> : null}
+                    {touchButtonOne > 1 && touchButtonTwo > 1 ? <h2 className="login-title"><img className="gengar" src={gengar} alt="" />1000% MATCH BABAY <img className="kirby" src={kirby} alt="" /></h2> : null}
+            <i onClick={closeVdayModal} className="fa-solid fa-xmark close-modal"></i>
           </div>
           <div className="modal-assessment-div">
-            <h2>I WAS MADE FOR LOVIN YOU BABY... YOU REALLY GET MY LOVE GUN RUNNIN HOT! WILL YOU BE MINE?</h2>
+                    {touchButtonOne === 1 && touchButtonTwo === 1 ? <h2>YOU SHOCK ME... YOU'RE PART OF THE PSYCHO CIRCUS IF YOU THINK WE'RE MEANT TO BE</h2> : null}
+                    {touchButtonOne > 1 && touchButtonTwo === 1 ? <h2>YOU SHOCK ME... YOU'RE PART OF THE PSYCHO CIRCUS IF YOU THINK WE'RE MEANT TO BE</h2> : null}
+                    {touchButtonOne === 1 && touchButtonTwo > 1 ? <h2>YOU SHOCK ME... YOU'RE PART OF THE PSYCHO CIRCUS IF YOU THINK WE'RE MEANT TO BE</h2> : null}
+                    {touchButtonOne > 1 && touchButtonTwo > 1 ? <h2>HOTTER THAN HELL, I WAS MADE FOR LOVIN YOU BABY... YOU REALLY GET MY LOVE GUN RUNNIN HOT! WILL YOU BE MINE?</h2> : null}
           </div>
         </div>
       </div>
